02accaf2f0bbca64c985192daa41d0b3de5dbef5
GJ Tiquia <gershom.tiquia@gmail.com>
2024-05-17 00:58:08 +0800

    feat: basic client server socket communication

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,24 +1,32 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css"
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+console.log("Client Running...")
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+console.log("Creating socket...");
+let socket = new WebSocket("ws://localhost:3000/");
+
+socket.onopen = function () {
+    console.log("Socket open.");
+
+    // TODO : Better send JSON messages instead
+    socket.send("Client socket opened!");
+
+    console.log("Message sent.")
+};
+
+socket.onmessage = function (message) {
+    console.log("Received server message:", message.data);
+};
+
+const buttonElement = document.getElementById("message-button");
+if (!buttonElement) {
+    throw new Error("No element with id 'message-button'!");
+}
+
+buttonElement.addEventListener("click", () => {
+
+    // TODO : Better send JSON messages instead
+    socket.send("Client button clicked!");
+
+    console.log("Message sent with button.")
+});
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,10 @@
 import path from "path"
+import http from "http"
 import express from "express"
+import ws from "ws"
 
 const app = express()
-const port = 3000
+const PORT = 3000
 
 // Global Middleware
 // app.use(cors()); // Enable all origins
@@ -11,6 +13,29 @@ const port = 3000
 const publicFilesPath = path.join(__dirname, "..", "..", "client", "dist")
 app.use(express.static(publicFilesPath))
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+let WSServer = ws.Server;
+let server = http.createServer();
+let wss = new WSServer({
+    server: server,
+    perMessageDeflate: false
 })
+
+// Mount Express App on HTTP Server
+server.on('request', app);
+
+// Listen to WebSocket events
+wss.on('connection', function connection(ws) {
+    console.log("Client connected!")
+
+    ws.on('message', function incoming(message) {
+        console.log(`Message received: ${message}`);
+
+        // TODO : send JSON messages instead for extensibility
+        ws.send(`Server received the message '${message}'`);
+    });
+});
+
+// Start listening on PORT
+server.listen(PORT, function () {
+    console.log(`Example app listening on port ${PORT}`)
+});
