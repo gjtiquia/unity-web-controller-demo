6ba7a4d069ab0caa2d069304d5d2d6358d47e091
GJ Tiquia <gershom.tiquia@gmail.com>
2024-05-17 00:28:52 +0800

    chore: vite client and express serving static files

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello World!")
+import path from "path"
+import express from "express"
+
+const app = express()
+const port = 3000
+
+// Global Middleware
+// app.use(cors()); // Enable all origins
+
+// Static File Server
+const publicFilesPath = path.join(__dirname, "..", "..", "client", "dist")
+app.use(express.static(publicFilesPath))
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
